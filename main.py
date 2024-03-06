16e3db836e416cb5cc06a89aac4a4eeaf9af2886
shwetha krishnamurthy <shwetha.krishnamurthy27@gmail.com>
2024-03-06 00:56:40 -0600

    Removed asking for Pinecone API Key

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ def main():
     # API Key
     eden_ai_api_key = st.text_input("Enter EdenAI API Key (required; Click here to create one: https://www.edenai.co/):", type='password')
     
-    pinecone_api_key = st.text_input("Enter Pinecone API Key (required):", type='password')
+    # pinecone_api_key = st.text_input("Enter Pinecone API Key (required):", type='password')
 
     os.environ['EDEN_AI_API_KEY'] = "Bearer " + eden_ai_api_key
-    os.environ['PINECONE_API_KEY'] = pinecone_api_key
+    # os.environ['PINECONE_API_KEY'] = pinecone_api_key
 
     # User input for the research paper's title
     paper_title = st.text_input("Enter the research paper title you want to understand:")
@@ -25,7 +25,7 @@ def main():
 
     # Button for submitting the query
     if st.button('Explain it to me!'):
-        if paper_title and eden_ai_api_key and pinecone_api_key:
+        if paper_title and eden_ai_api_key: # and pinecone_api_key:
             # Generating the summary based on the API key, paper title, and expertise level
             # explanation = query_explanation.get_query_explanation(paper_title, expertise_level)
             explanation = get_summary(search_papers("Large Language Models"))
